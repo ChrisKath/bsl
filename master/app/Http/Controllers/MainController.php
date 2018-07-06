737137c8f943a2fedff0b0f7eb8a5da1d3911179
Eye TAP10 <eye@tap10.com>
2018-07-06 17:30:56 +0700

    redirect to app

--- master/app/Http/Controllers/MainController.php
+++ master/app/Http/Controllers/MainController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-use Browser;
 use Illuminate\Http\Request;
 use App\Url as WATCH;
 use App\Click as CLICK;
@@ -25,10 +24,10 @@ class MainController extends Controller {
     
     
     return $this->state($key) ? 
-                $this->detectFB($this->href) ? $this->fb 
-                  :$this->href
-                 // redirect()->away($this->href) ? 
-                 : view('404');
+            $this->detectFB($this->href) ? 
+              view('app', ['href' => $this->href , 'fb' => $this->fb])
+                : redirect()->away($this->href)
+                  : view('404');
   }
 
   /**
@@ -70,49 +69,61 @@ class MainController extends Controller {
     $click->save();
   }
 
+  /**
+  * Display a listing of the resource.
+  *
+  * @param String $key
+  * @return ViewPoint,Redirect
+  **/
+  public function test ($key) {
+    if (
+      in_array($this->cute($key), $this->ignoreVueRoute)
+    ) return view('root');
+    
+    
+    return $this->state($key) ? 
+            $this->detectFB($this->href) ? 
+              view('app', ['href' => $this->href , 'fb' => $this->fb])
+                : redirect()->away($this->href)
+                  : view('404');
+  }
   public function detectFB($url){
-    // $user_agent = \Request::header('User-Agent');
-    $user_agent = 'iPad';
+    $user_agent = \Request::header('User-Agent');
+    // $user_agent = 'ipad';
     $regex_fb = "/(?:https?:\/\/)?(?:(?:www\.|m\.|touch\.)?(?:facebook|fb)\.(?:com|me)\/)(.*)/i";
     if(!preg_match('/iPad|iPhone|android/i',$user_agent,$device)) return false;
     if(!preg_match_all($regex_fb,$url,$m)) return false; // 0 : match , 1 : match string , 2 : after match
    
     $item = array(
       'package'=>'com.facebook.katana', // android
-      'scheme' => 'fb' // ios
-    );
+      'scheme' => 'fb'
+    );     
     switch (1) {
-      case preg_match_all('/(?:events\/)(\d*)(\/.*)/i',$m[1][0],$s):   
+      case preg_match_all('/(?:events\/)(\d*)(?:\/|\?)?(.*)/i',$m[1][0],$s):   
         $item['host'] = 'event/'.$s[1][0];
         $item['path'] = 'event?id='.$s[1][0];        
         break;
-      case preg_match_all('/(?:groups\/)(\d*)(\/.*)/i',$m[1][0],$s):
+      case preg_match_all('/(?:groups\/)(\d*)(?:\/|\?)?(.*)/i',$m[1][0],$s):
         $item['host'] = 'group/'.$s[1][0];
         $item['path'] = 'group?id='.$s[1][0];
         break;
-      case preg_match_all('/(?:profile\.php\?id=)(\d*)(\/.*)/i',$m[1][0],$s): 
+      case preg_match_all('/(?:profile\.php\?id=)(\d*)(?:\/|\?)?(.*)/i',$m[1][0],$s): 
         $item['host'] = 'profile/'.$s[1][0];
         $item['path'] = 'profile/'.$s[1][0];
         break;
-      case preg_match_all('/(?:pages\/|pg\/).*\-(\d*)(\/.*)/i',$m[1][0],$s):
+      case preg_match_all('/(?:pages\/|pg\/).*\-(\d*)(?:\/|\?)?(.*)/i',$m[1][0],$s):
         $item['host'] = 'page/'.$s[1][0];        
         $item['path'] = 'profile/'.$s[1][0];   
         break; 
-      case preg_match_all('/(.*)(?:\/)(.*)/i',$m[1][0],$s):
-        $fb = FB::where('name',$s[1][0])->first();
-        if($fb->count()){
+      case preg_match_all('/(\w*)(?:\/|\?)?(.*)/i',$m[1][0],$s) > 0:
+        $fb = FB::whereRaw('LOWER(name)=\''.strtolower($s[1][0]).'\'')->first();
+        if($fb){
           $item['host'] = 'page/'.$fb->id;        
           $item['path'] = 'profile/'.$fb->id;   
-        }else{
-          return false;
-        }
+        }else return false;        
         break;
-      default:        
+      default: 
         return false;
-    }
-    if(isset($s[2][0]) && $s[2][0] != '/'){
-      $item['host'].= $s[2][0];
-      $item['path'].='&'.substr($s[2][0],1);
     }
     $this->fb = $this->deeplink($item,$device[0]); 
     return true;
---
+++ master/resources/views/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta name="robots" content="noindex, nofollow">
+	<meta name="google" value="notranslate">
+  <meta charset="UTF-8">
+
+  <title>TAP Technology | Digital Studio - Official Site</title>
+
+  <link rel="shortcut icon" href="/static/ico.png">
+  <script type = "text/javascript">
+    setTimeout(function () { window.location = "{{ $href }}"; }, 25);
+    window.location = "{{ $fb }}";
+  </script>
+</head>
+<body>
+  <a href="{{ $fb }}">FB :: {{ $fb }}</a><br>
+  <a href="{{ $href }}">URL :: {{ $href }}</a>
+</body>
+</html>
+
+<!-- *******************************************************
+
+This website is produced by TAP Technology Company Limited.
+e-mail: info@tap10.com
+website: www.tap10.com
+
+************************************************************ -->
